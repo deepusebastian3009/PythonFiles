1a09959af9e7552a88309ea3c63315cc2066d7ff
deepusebastian3009 <83339003+deepusebastian3009@users.noreply.github.com>
2021-05-05 17:40:00 +0530

    Add files via upload

---
+++ B43_Python Fundamentals.py
@@ -0,0 +1,397 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Introduction to Python classes:
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+class: It is a high level blue print of the program.
+    
+    We have to use a keyword class to define it and also we neeed to use title case for declaring the class name.
+    
+    methods : A function written inside the class is called as method.
+        
+    attribute : A variable written inside the class is called an attribute
+            
+    self : : Aself is a temporary placeholder for an object.
+                    
+    object or instance : An object is an acces or entry point to the class methods.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#create a Greet class and have the below methods for the same
+# methods below
+#1. userinput
+#2.displayuser
+#3.greetuser
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+class Greet:
+    """creating the greet class and its methods"""   #doc string
+    
+    def userinput(self,username):     #methodname
+        self.username=username   #same format   #attributes   #test.usename="Praveen"
+        self.age=age   #format we have to follow   #attributes
+        
+        def displayuser(self):
+            print(self.username)
+            def greetuser(self):  #method
+                print(f"Hello, {self.username}"")
+            
+            
+
+
+# In[ ]:
+
+
+test=Greet()     #object assignment to class
+
+
+# In[ ]:
+
+
+test.userinput('Praveen')
+
+
+# In[2]:
+
+
+test.displayuser()
+
+
+# In[3]:
+
+
+test.greetuser()
+
+
+# In[ ]:
+
+
+xobj=Greet()
+
+
+# In[ ]:
+
+
+xobj.userinput('Suhitha')
+
+
+# In[5]:
+
+
+xobj.displayuser()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
