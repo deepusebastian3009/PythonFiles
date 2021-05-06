1230364b2d20550dd20db9ad0039f16f95451608
deepusebastian3009 <83339003+deepusebastian3009@users.noreply.github.com>
2021-05-06 18:26:08 +0530

    Add files via upload

---
+++ B43_Python Fundamentals(3).py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Continuation with Python classes:
+
+
+# In[ ]:
+
+
+class------Itis a high level blue print of the program, we have class keyword for defining it.
+
+methods-------A function written inside the class is called as methods.
+
+attributes-----A variable written inside the class is called as attributes.
+
+self------It is a temp placeholder.
+
+objects-----It is an 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to constructor : It is a special method also called as magical method.
+    It is used to initialize attributes and methods automatically.
+    
+    How to define the constructor:
+        
+        def_int_()     #This is the syntax for defining the constructor.
+        
+        # It is considered as the best practice for using constructors in program.
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#scenario : Create a Dog class with the parameters as name, age and define the below methods
+
+#1. init method for accepting the input
+#2. jump
+#3. rollover
+#4. sit
+#5. sleep
+#6. details
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+class Dog:
+    ""creating a Dog class and definin the methods""
+    
+    def_init(self,name,age)   #constructor(method)
+    self.name=name    #attributes   #test.name='scooby'
+    self.age=age      #attriutes
+    print("It got automatically executed")
+    
+    def jump(self):
+        print(f"{self.name}is now jumping on the command")
+        
+         def rollover(self):
+        print(f"{self.name}is now rolling over")
+        
+         def sit(self):
+        print(f"{self.name}is now sitting on the command")
+        
+         def sleep(self):
+        print(f"{self.name}is now sleeping")
+        
+         def details(self):
+        print(f"I have a dog named{self.name}")
+        print(f"{self.name} is now {self.age} years old")
+
+
+# In[1]:
+
+
+test=Dog('scooby',2)     #directly assign the args 
+
+
+# In[2]:
+
+
+test.jump()
+
+
+# In[3]:
+
+
+test.rollover()
+
+
+# In[4]:
+
+
+test.sit()
+
+
+# In[5]:
+
+
+test.sleep()
+
+
+# In[6]:
+
+
+test.details()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
